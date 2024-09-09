38412b092cbabb592440011919ac926341fa8278
diego <diegozaratean@gmail.com>
2024-09-09 18:16:49 +0000

    finish data modeling

--- src/models.py
+++ src/models.py
@@ -7,6 +7,48 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class Empresa(Base):
+    __tablename__ = 'empresa'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    ciudad = Column(String(250), nullable=False)
+    slogan = Column(String(250), nullable=False)
+
+
+
+class Videojuego(Base):
+    __tablename__ = 'videojuego'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    genero = Column(String(250), nullable=False)
+    rate = Column(Integer, nullable=False)
+    empresa_id = Column(Integer, ForeignKey('empresa.id'))
+    empresa = relationship(Empresa)
+
+
+class Consola(Base):
+    __tablename__ = 'consola'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    precio = Column(Integer, nullable=False)
+
+
+class ConsolaVideojuego(Base):
+    __tablename__ = 'consola_videojuego'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    consola_id = Column(Integer, ForeignKey('consola.id'))
+    consola = relationship(Consola)
+    videojuego_id = Column(Integer, ForeignKey('videojuego.id'))
+    videojuego = relationship(Videojuego)
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
